3c9fedee754dbc1f83b383af2683a204087df8fc
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-07-30 20:32:06 -0300

    Add files via upload

---
+++ survey-form.js
@@ -0,0 +1,8 @@
+
+// Scroll Reveal
+
+ScrollReveal().reveal(' .menu-photos', { delay: 350 });
+ScrollReveal().reveal(' .form-section', { delay: 350 });
+ScrollReveal().reveal(' .container02', { delay: 350 });
+ScrollReveal().reveal(' .container03', { delay: 350 });
+ScrollReveal().reveal(' .container04', { delay: 350 });
